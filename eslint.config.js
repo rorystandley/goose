729c7922ccd35db22f0049b743d301513be5284d
Rory Standley <rorystandley@gmail.com>
2026-03-15 18:23:29 +0000

    chore: improve CI process

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/', 'coverage/'],
+  },
+  {
+    ...js.configs.recommended,
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
